c6af6d611382cab39ff6f3cbaf6a4eb515edd5a7
Everton Tenorio <etenoriodev@gmail.com>
2025-04-26 23:03:44 -0300

    refactor: convert validate-projects.js to ESM for Vite 6.3.3 compatibility

--- scripts/validate-projects.js
+++ scripts/validate-projects.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Workaround para __dirname em ESM
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const projectsDir = path.join(__dirname, '../projects');
 
